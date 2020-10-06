18edeb382e3531bc047b06a43f206b91e2edc2bf
LswaN58 <lwswanson2@wisc.edu>
2020-10-06 07:17:23 -0500

    Rename scripts.js -> files_page.js, and realtime.js -> dashboard_page.js, for clarity.

--- index.php
+++ index.php
@@ -132,10 +132,10 @@
 <link href="https://cdnjs.cloudflare.com/ajax/libs/c3/0.4.10/c3.min.css" rel="stylesheet" />
 <script src="https://cdnjs.cloudflare.com/ajax/libs/d3/3.5.6/d3.min.js"></script>
 <script src="https://cdnjs.cloudflare.com/ajax/libs/c3/0.4.10/c3.min.js"></script>
-<script src="scripts.js"></script>
+<script src="files_page.js"></script>
 <script src="realtime_config.js"></script>
 <script src="RTServer.js"></script>
-<script src="realtime.js"></script>
+<script src="dashboard_page.js"></script>
 <script src="games_list.js"></script>
 <script>
 $(function() {
