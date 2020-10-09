e5a72216e5a88ecd9325e78984a7dd46bd745339
LswaN58 <lwswanson2@wisc.edu>
2020-10-09 07:48:21 -0500

    Dashboard page now manages Dashboard object. Also, missed removing visualizer code earlier.

--- dashboard_page.js
+++ dashboard_page.js
@@ -9,6 +9,10 @@ var PRINT_TRACE = true;
 function onload()
 {
   // Create a PlayerList instance for tracking state, and start the refresh loop.
+  var dashboard = new PlayerDashboard();
+  var NewSelectionHandler = function(session_id, player_id, game_id) {
+    dashboard.DisplaySession(session_id, player_id, game_id);
+  }
   sess_list = new PlayerList();
   rt_change_games(sess_list, "LAKELAND");
   if (rt_config.custom_title !== null)
@@ -114,80 +118,6 @@ function setMinus(A, B) {
   return new Set([...A].filter(x => !B.has(x)));
 }
 
-class Visualizer
-{
-  static createMultiBarChart(vals, bind_to_id) {
-    let cols = [];
-    for (let i=0; i < vals.length; i++) {
-      let val = vals[i];
-      cols.push([`data${i}`, val]);
-    }
-    return c3.generate(
-      {
-        bindto: "#" + bind_to_id,
-        data: {
-          columns: cols,
-          type: 'bar'
-        },
-        axis : {
-          y : {
-              tick: { format: d3.format('d') },
-          }
-        },
-        size: {
-          height: 200-70,
-          width: .28*200
-        },
-        legend: { show: false }
-      });
-  }
-
-  static createBarChart(val, bind_to_id) {
-    return c3.generate(
-      {
-        bindto: "#" + bind_to_id,
-        data: {
-          columns: [
-            ['data1', val]
-          ],
-          type: 'bar'
-        },
-        axis : {
-          y : {
-              tick: { format: d3.format('d') },
-          }
-        },
-        size: {
-          height: 200-70,
-          width: .28*200
-        },
-        legend: { show: false }
-      });
-  }
-
-  static createGaugeChart(val, bind_to_id, reverse_color) {
-    return c3.generate(
-      {
-        bindto: "#" + bind_to_id,
-        data: {
-          columns: [
-            ['data1', val]
-          ],
-          type: 'gauge'
-        },
-        color: {
-          pattern: reverse_color ? ['green', 'yellow', 'orange', 'red'] : ['red', 'orange', 'yellow', 'green'], // the three color levels for the percentage values.
-          threshold: {
-              values: reverse_color ? [50, 80, 90, 100] : [10, 20, 50, 100]
-          }
-        },
-        size: {
-          height: 200-140,
-          width: .28*200
-        }
-      });
-  }
-}
 
 function playstats_message(msg){
   let message = document.createElement("p")
