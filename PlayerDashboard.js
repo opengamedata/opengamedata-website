2d1e5bc4bf0d4667e7512a21a1b2453ce21d7dbd
LswaN58 <lwswanson2@wisc.edu>
2020-10-09 06:34:42 -0500

    Move code for handling list of players and dashboard itself to own files.

---
+++ PlayerDashboard.js
@@ -0,0 +1,199 @@
+class PlayerDashboard
+{
+  constructor() {
+    this.active_game = "None";
+    this.selected_session_id = -1;
+    this.require_player_id = document.getElementById("require_pid").checked;
+    this.statistics_NA_msg = false;
+    this.request_count = 0;
+    this.feature_boxes = [];
+    this.model_boxes = [];
+  }
+
+  clear() {
+    // here we'll just clear the stuff displayed in the model area.
+    let playstats = document.getElementById("playstats");
+    while (playstats.firstChild) {
+      playstats.removeChild(playstats.firstChild);
+    }
+    this.selected_session_id = -1;
+    this.statistics_NA_msg = false;
+  }
+
+  /**
+   * Function to set up display of models for a given session.
+   * Once this has been run, another function can be used to update
+   * the model values in place (without removing and replacing elements).
+   * @param {*} session_id The id of the session to display.
+   */
+  DisplaySession(session_id, player_id, game_id) {
+    let that = this;
+    this.selected_session_id = session_id;
+    this.active_game = game_id;
+    let playstats = document.getElementById("playstats");
+    let message = document.createElement("h4")
+    let player_msg = !["", "null"].includes(player_id) ? " (Player "+player_id+")" : '';
+    message.appendChild(document.createTextNode("Session "+session_id+player_msg));
+    message.style.width = "-webkit-fill-available";
+    playstats.appendChild(message);
+    let feature_request_list = this.create_feature_request_list();
+    let features_handler = function(result) {
+      let features_raw = that.parseJSONResult(result);
+      let features_parsed = features_raw[that.selected_session_id];
+      // loop over all models, adding to the UI.
+      for (let feature_name in features_parsed) {
+        let next_box = ModelBox(feature_name, feature_request_list[feature_name]["name"], playstats);
+        next_box.update(features_parsed, feature_request_list);
+        that.feature_boxes.push(next_box);
+      }
+
+      if(features_raw === 'null'){
+        playstats_message('No features available.')
+      } 
+    };
+    let model_request_list = this.create_model_request_list();
+    let models_handler = function(result) {
+      let models_raw = that.parseJSONResult(result);
+      let model_list = models_raw[that.selected_session_id]
+      // loop over all models, adding to the UI.
+      for (let model_name in model_list) {
+        let next_box = ModelBox(model_name, model_list[model_name]["name"], playstats);
+        next_box.update(models_parsed, model_request_list);
+        that.model_boxes.push(next_box);
+        that.populateModelBox(model_name, model_list);
+      }
+      if(models_raw === 'null'){
+        playstats_message('No models available.')
+      }
+    };
+    try {
+      Server.get_models_by_sessID(models_handler, this.selected_session_id, this.active_game, SIM_TIME, Object.keys(model_request_list));
+      Server.get_features_by_sessID(features_handler, this.selected_session_id, this.active_game, SIM_TIME, Object.keys(feature_request_list));
+    }
+    catch(err) {
+      console.log(err.message);
+      if (PRINT_TRACE)
+      {
+        console.trace();
+      }
+    }
+  }
+
+  /**
+   * Function to update the model values for a displayed session.
+   * This assumes a session has been selected, and its id stored in
+   * the SessionList selected_session_id variable.
+   */
+  Refresh()
+  {
+    let that = this;
+    let feature_request_list = this.create_feature_request_list();
+    let features_handler = function(result) {
+      // console.log(`Got back models: ${result}`);
+      let features_raw = that.parseJSONResult(result);
+      let features_parsed = features_raw[that.selected_session_id]
+      // After getting the feature values, loop over whole list,
+      // updating values.
+      for (let feature_name in features_parsed) {
+        let feat_span = document.getElementById(feature_name);
+        if (feat_span == null) {
+          that.createFeatureBox(feature_name, feature_request_list[feature_name]["name"], playstats);
+        }
+        that.populateFeatureBox(feature_name, features_parsed, feature_request_list);
+      }
+      that.request_count--;
+    };
+    let model_request_list = this.create_model_request_list();
+    console.log("model list:");
+    console.log(model_request_list);
+    let models_handler = function(result) {
+      // console.log(`Got back models: ${result}`);
+      let models_raw = that.parseJSONResult(result);
+      let model_list = models_raw[that.selected_session_id]
+      // After getting the model values, loop over whole list,
+      // updating values.
+      for (let model_name in model_list) {
+        let pred_span = document.getElementById(model_name);
+        if (pred_span == null) {
+          that.createModelBox(model_name, model_list[model_name]["name"], playstats);
+        }
+        that.populateModelBox(model_name, model_list);
+      }
+      that.request_count--;
+    };
+    try {
+      if (this.request_count < rt_config.max_outstanding_requests)
+      {
+        this.request_count++;
+        Server.get_models_by_sessID(models_handler, this.selected_session_id, this.active_game, SIM_TIME, Object.keys(model_request_list));
+        this.request_count++;
+        Server.get_features_by_sessID(features_handler, this.selected_session_id, this.active_game, SIM_TIME, Object.keys(feature_request_list));
+      }
+      else
+      {
+        console.log(`Request count is ${this.request_count}, not making another.`);
+      }
+    }
+    catch(err) {
+      console.log(err.message);
+      if (PRINT_TRACE)
+      {
+        console.trace();
+      }
+    }
+  }
+
+  create_feature_request_list()
+  {
+    let feature_request_list = {};
+    // Special case for Lakeland, as at the moment I've got two sets of features set up.
+    // Eventually, there should just be the one.
+    if(this.active_game === 'LAKELAND'){
+      if (NEW_FEATURE_SET) {
+        feature_request_list = active_features["LAKELAND"]["new_set"]
+      }
+      else {
+        feature_request_list = active_features["LAKELAND"]["old_set"]
+      }
+    }
+    else {
+      feature_request_list = active_features[this.active_game]
+    }
+    return feature_request_list;
+  }
+
+  create_model_request_list()
+  {
+    let model_request_list = active_models[this.active_game];
+    return model_request_list;
+  }
+
+  parseJSONResult(json_result)
+  {
+    let ret_val = 'null';
+    try
+    {
+      let models_raw = JSON.parse(json_result);
+      ret_val = models_raw;
+    }
+    catch (err)
+    {
+      console.log(`Could not parse result as JSON:\n ${json_result}`);
+      console.log(`Full error: ${err.toString()}`);
+      if (PRINT_TRACE)
+      {
+        console.trace();
+      }
+      ret_val = {"message": json_result.toString()};
+    }
+    finally
+    {
+      return ret_val;
+    }
+  }
+}
+
+class PlayerCard
+{
+
+}
---
+++ PlayerList.js
@@ -0,0 +1,194 @@
+
+/**
+ * Class to manage data related to sessions for each game.
+ * This is responsible for maintaining a list of sessions,
+ * as well as displaying the data in the view.
+ * Technically, if this were a more complicated page,
+ * we might want to split out the data from the display,
+ * but this works well for what it is.
+ */
+class PlayerList
+{
+  /**
+   * Constructor for the SessionList class
+   * Sets up variables to track the active game, lists of sessions ids
+   * (one list is what's active, the other is what's actively displayed),
+   * and a selected ID (for detailed display).
+   */
+  constructor() {
+    this.active_game = "LAKELAND";
+    this.active_sessions = [];
+    this.active_session_ids = [];
+    this.displayed_session_ids = [];
+    this.selected_session_id = -1;
+    this.require_player_id = document.getElementById("require_pid").checked;
+    this.statistics_NA_msg = false;
+    this.request_count = 0;
+    this.selected_session_dash = new SessionDashboard()
+    this.refreshActiveSessionList();
+  }
+
+  /**
+   * Function to retrieve a list of currently active sessions.
+   * First, we call the CGI backend to get the list.
+   * In the handler, the returned list updates the SessionList data,
+   * and then makes a further call to refresh the display list.
+   */
+  refreshActiveSessionList() {
+    let that = this;
+    function active_sessions_handler(result) {
+      let parsed_sessions = 'null';
+      try
+      {
+        parsed_sessions = JSON.parse(result);
+      }
+      catch (err)
+      {
+        console.log(`Could not parse result as JSON:\n ${result}`);
+        if (PRINT_TRACE)
+        {
+          console.trace();
+        }
+        that.request_count--;
+        return;
+      }
+      that.active_sessions = parsed_sessions;
+      console.log('Refreshed session IDs:');
+      console.log(that.active_sessions);
+      that.active_session_ids = Array.from(Object.keys(that.active_sessions));
+      that.refreshDisplayedSessionList();
+      that.request_count--;
+    };
+    if (this.request_count < rt_config.max_outstanding_requests)
+    {
+      this.request_count++;
+      Server.get_all_active_sessions(active_sessions_handler, this.active_game, this.require_player_id, SIM_TIME);
+    }
+    else
+    {
+      console.log(`Request count is ${this.request_count}, not making another.`);
+    }
+  }
+
+  /**
+   * Function to update the list of displayed session IDs.
+   * This is done in a way that preserves the order of session IDs
+   * as much as possible.
+   */
+  refreshDisplayedSessionList() {
+    let display_set = new Set(this.displayed_session_ids);
+    let active_set = new Set(this.active_session_ids);
+    let remove_set = setMinus(display_set, active_set); // subtract active from display to get inactives, which are currently displayed.
+    let add_set    = setMinus(active_set, display_set); // subtract display from active to get new sessions, which were not displayed yet.
+    let session_list_area = document.getElementById("session_list");
+    if(this.displayed_session_ids.length == 0){
+      session_list_area.innerHTML = '';
+    }
+
+    // First, refresh what's in the list.
+    let child_nodes = Array.from(session_list_area.children);
+    for (let session_link_num in child_nodes) {
+      let session_link = child_nodes[session_link_num];
+      let session_id = session_link.id;
+      // let session_link = child_nodes[`div_${session_id}`];
+      // A) If object is in remove set, remove it.
+      if (remove_set.has(session_id)) {
+        session_link.remove();
+        if (this.selected_session_id == session_link.id) { this.clearSelected(); }
+      }
+      // B) Else, update the max and current levels.
+      else {
+        this.populateDisplayedSession(session_id);
+      }
+    }
+    // loop over all newly active sessions, adding them to the list.
+    for (let id of add_set) {
+      let session_id = id;
+      let player_id = this.active_sessions[session_id]["player_id"];
+      // start constructing the element
+      let session_div = this.constructDisplayedSession(session_id, player_id);
+      session_list_area.appendChild(session_div);
+      this.populateDisplayedSession(session_id);
+    }
+    this.displayed_session_ids = [...this.active_session_ids]; // at this point, these should theoretically be the same.
+    if(this.displayed_session_ids.length == 0){
+      let message = document.createElement("p")
+      let player_id_msg = this.require_player_id ? " Try viewing sessions without player IDs." : "";
+      message.appendChild(document.createTextNode("No sessions currently available."+player_id_msg))
+      session_list_area.appendChild(message);
+    }
+  }
+
+  constructDisplayedSession(session_id, player_id)
+  {
+    let game_themes = {"CRYSTAL": "seascape", "WAVES":"daisygarden", "JOWILDER": "heatwave", "LAKELAND": "summerwarmth"}
+    let that = this; // needed for onclick handler.
+
+    let session_div = document.createElement("div");
+    session_div.id = session_id;
+    let avatar_img = document.createElement('img');
+    avatar_img.src = 'http://tinygraphs.com/spaceinvaders/' + session_id + `?theme=${game_themes[this.active_game]}&numcolors=4`;
+    session_div.appendChild(avatar_img);
+    let session_link = document.createElement("a");
+    session_link.onclick = function() { that.displaySelectedSession(session_id); return false;}
+    session_link.innerText = !["", "null"].includes(player_id) ? player_id : session_id;
+    session_link.href = `#${session_id}`;
+    session_div.appendChild(session_link);
+    let cur_level_div = document.createElement("div");
+    cur_level_div.id = `cur_level_${session_id}`;
+    session_div.appendChild(cur_level_div);
+    let max_level_div = document.createElement("div");
+    max_level_div.id = `max_level_${session_id}`;
+    session_div.appendChild(max_level_div);
+    session_div.appendChild(document.createElement("br"));
+
+    let alert_msg = document.createElement("span");
+    alert_msg.id = `idle_${session_id}`;
+    alert_msg.innerText = "Inactive";
+    alert_msg.classList.add("player_inactive");
+    session_div.appendChild(alert_msg);
+
+    return session_div
+  }
+
+  populateDisplayedSession(session_id) {
+    let cur_level_div = document.getElementById(`cur_level_${session_id}`);
+    cur_level_div.innerText = `current: ${this.active_sessions[session_id]["cur_level"].toString()}`;
+    let max_level_div = document.getElementById(`max_level_${session_id}`);
+    max_level_div.innerText = `max: ${this.active_sessions[session_id]["max_level"].toString()}`;
+    let inactive_span = document.getElementById(`idle_${session_id}`);
+    if (this.active_sessions[session_id]["idle_time"] > 60)
+    {
+      inactive_span.style.display = "inline";
+    }
+    else
+    {
+      inactive_span.style.display = "none";
+    }
+  }
+
+  displaySelectedSession(session_id) {
+    this.clearSelected();
+    let player_id = this.active_sessions[session_id]['player_id']
+    this.selected_session_dash.DisplaySession(session_id, player_id, this.active_game);
+    this.selected_session_id = session_id;
+  }
+
+  refreshSelectedSession() {
+    this.selected_session_dash.Refresh();
+  }
+
+  /**
+   * Simple function to clear out data display for a selected session.
+   * This is mostly intended for when switching to a new session or switching
+   * to another game entirely.
+   */
+  clearSelected() {
+    this.selected_session_dash.clear()
+  }
+}
+
+class PlayerCard
+{
+
+}
