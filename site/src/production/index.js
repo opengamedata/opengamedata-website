9c6f375fd605365502b3b84041e9e348645db52e
Luke Swanson <lwswanson2@wisc.edu>
2022-07-08 09:51:32 -0500

    Latest version of index.

--- site/src/production/index.js
+++ site/src/production/index.js
@@ -1,24 +1,29 @@
 // import React from 'react';
 // import ReactDOM from 'react-dom';
-// import { BrowserRouter } from "react-router-dom";
-import './index.css';
-import App from './App.js';
-import reportWebVitals from './reportWebVitals.js';
+import { BrowserRouter } from "react-router-dom";
+// import './index.css';
+// import App from './App.js';
+// import reportWebVitals from './reportWebVitals.js';
 
 var root_elem = document.getElementById('dashroot');
-console.log('starting to run index.js, root element is ' + root_elem);
+console.log("starting to run index.js, root element is " + root_elem);
 ReactDOM.render(React.createElement(
   React.StrictMode,
   null,
   React.createElement(
-    'div',
+    BrowserRouter,
     null,
-    'A div in index.js'
+    React.createElement(
+      "div",
+      null,
+      "A div in index.js"
+    ),
+    React.createElement(App, null)
   )
 ), root_elem);
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
-console.log('ran index.js');
+// reportWebVitals();
+console.log("ran index.js");
