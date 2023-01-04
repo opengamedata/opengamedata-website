5e6608c6f664c74293d43627be9c38ba7310eaa8
Luke Swanson <lwswanson2@wisc.edu>
2023-01-04 10:35:03 -0600

    Explicitly import React.

--- src/components/buttons/LargeButton.js
+++ src/components/buttons/LargeButton.js
@@ -1,4 +1,4 @@
-
+import { React } from 'react';
 
 export default function LargeButton({ selected, onClick, label }) {
     const baseStyle = "px-7 py-2 my-3 border border-slate-800 transition-colors ease-in-out duration-300 font-medium text-xl hover:text-yellow-300 hover:bg-slate-800 rounded-md text-sm "
