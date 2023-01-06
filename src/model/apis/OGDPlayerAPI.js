7826a8086232e06f4f91061fc1d52b66ab65b66e
Luke Swanson <lwswanson2@wisc.edu>
2023-01-05 22:32:37 -0600

    Handle possible missing player ID or start/end dates.

--- src/model/apis/OGDPlayerAPI.js
+++ src/model/apis/OGDPlayerAPI.js
@@ -1,18 +1,35 @@
 import { API_ORIGIN } from '../../config';
-import * as d3 from 'd3';
+import { PlayerSelectionOptions } from '../../controller/SelectionOptions';
 
 export class OGDPlayerAPI {
-   static fetchPlayer(player_id, filter_options, metrics) {
-      const path = OGDPlayerAPI.getURLPath(player_id, filter_options, metrics)
+   /**
+    * @param {PlayerSelectionOptions} selection_options 
+    * @param {string[]} metrics 
+    * @returns {Promise<Response>}
+    */
+   static fetch(selection_options, metrics) {
+      const path = OGDPlayerAPI.getURLPath(selection_options, metrics)
       return       fetch(path)
    }
 
-   static getURLPath = (player_id, filter_options, view_metrics) => {
+   /**
+    * @param {PlayerSelectionOptions} selection_options 
+    * @param {string[]} view_metrics 
+    * @returns {URL}
+    */
+   static getURLPath = (selection_options, view_metrics) => {
       let searchParams, urlPath
       // construct url path and params
-      urlPath = `game/${filter_options.game_name}/player/${player_id}/metrics`;
+      if (selection_options.player_ids.length === 0) {
+         throw RangeError("No values found in selection_options.player_ids")
+      }
+      // TODO: replace this with ability to call the PlayerListAPI, not just PlayerAPI
+      if (selection_options.player_ids.length > 1) {
+         console.warn(`selection_options.player_ids had ${selection_options.player_ids.length} ids, only retrieving the first in PlayerAPI`);
+      }
+      urlPath = `game/${selection_options.game_name}/player/${selection_options.player_ids[0]}/metrics`;
       searchParams = new URLSearchParams({
-         metrics: view_metrics
+         metrics: `[${view_metrics}]`
       });
 
       // fetch by url
--- src/model/apis/OGDPopulationAPI.js
+++ src/model/apis/OGDPopulationAPI.js
@@ -1,17 +1,33 @@
 import { API_ORIGIN } from '../../config';
+import { PopulationSelectionOptions } from '../../controller/SelectionOptions';
 
 export class OGDPopulationAPI {
-   static fetch(filter_options, metrics) {
-      const path = OGDPopulationAPI.getURLPath(filter_options, metrics)
+   /**
+    * @param {PopulationSelectionOptions} selection_options 
+    * @param {string[]} metrics 
+    * @returns {Promise<Response>}
+    */
+   static fetch(selection_options, metrics) {
+      const path = OGDPopulationAPI.getURLPath(selection_options, metrics)
       return       fetch(path);
    }
 
-   static getURLPath = (filter_options, metrics) => {
+   /**
+    * @param {PopulationSelectionOptions} selection_options 
+    * @param {string[]} metrics 
+    * @returns {URL}
+    */
+   static getURLPath = (selection_options, metrics) => {
       // construct url path and params
-      const urlPath = `game/${filter_options.game_name}/metrics`;
+      const urlPath = `game/${selection_options.game_name}/metrics`;
+      if (selection_options.start_date === null) {
+         console.warn("selection_options.start_date was null! Defaulting to ")
+      }
+      let start_date = selection_options.start_date || new Date();
+      let end_date   = selection_options.end_date   || new Date();
       const searchParams = new URLSearchParams({
-         start_datetime: encodeURIComponent(filter_options.start_date) + 'T00:00',
-         end_datetime: encodeURIComponent(filter_options.end_date) + 'T23:59',
+         start_datetime: encodeURIComponent(start_date.toISOString().split('T')[0]) + 'T00:00',
+         end_datetime:   encodeURIComponent(  end_date.toISOString().split('T')[0]) + 'T23:59',
          metrics: `[${metrics}]`
       });
 
