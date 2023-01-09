415b33af7ef5299a3db525f1e527493b53a5781d
Luke Swanson <lwswanson2@wisc.edu>
2023-01-09 11:07:05 -0600

    Fix up a couple imports after moving.

--- src/index.js
+++ src/index.js
@@ -1,8 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { BrowserRouter } from "react-router-dom";
-import './view/index.css';
-import App from './App';
+import './index.css';
+import App from './view/App';
 import reportWebVitals from './reportWebVitals';
 
 ReactDOM.render(
