85cbf9c0c1f9a664d7c7c8b72822185018d6c7d0
soechting <soechting@wisc.edu>
2023-03-08 16:19:13 -0600

    use local chart.js

--- site/includes/chart.php
+++ site/includes/chart.php
@@ -2,7 +2,5 @@
     <canvas id="activityChart"></canvas>
 </div>
 
-<!-- <script src="../../node_modules/chart.js/chart.umd.js"></script> -->
-<!-- TODO: update this to pull from the local install, probably need to update something in gulpfile.js -->
-<script src="https://cdn.jsdelivr.net/npm/chart.js"></script>
+<script src="../assets/scripts/chart.umd.js"></script>
 <script type="module" src="../assets/scripts/chart.js"></script>
