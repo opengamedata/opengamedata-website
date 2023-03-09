98d964f34dfcb9e8eac19bbc9eb5f66d87e461f3
soechting <soechting@wisc.edu>
2023-03-09 08:09:12 -0600

    get chart data from API and update chart

--- site/assets/scripts/chart.js
+++ site/assets/scripts/chart.js
@@ -1,68 +1,88 @@
-// chart data
-// TODO=> get this via API
-var sessionsByDay = [340, 3034, 504, 2333, 340, 3034, 504, 2333, 340, 3034, 504, 2333, 340, 3034, 504, 2333, 340, 3034, 504, 2333, 340, 3034, 504, 2333, 340, 3034, 504, 2333, 340, 3034, 504];
-var hoverLabels = [];
+var chartInstance = null;
 
-// create labels, these are used in the hover
-sessionsByDay.forEach((element, index) => {
-    hoverLabels.push("March " + (index + 1));  // TODO: Update March with month variable
-});
+function createChart (sessionsByDay, month) {
+    var hoverLabels = [];
+    var displaySessions = [];
 
-const ctx = document.getElementById('activityChart');
+    for (const [key, value] of Object.entries(sessionsByDay)) {
+        var index = parseInt(key);
+        hoverLabels[index - 1] = month + " " + index;
+        displaySessions[index - 1] = value;
+    }
 
-new Chart(ctx, {
-    type: 'bar',
-    data: {
-        labels: hoverLabels,
-        datasets: [{
-            data: sessionsByDay,
-            borderWidth: 1,
-            backgroundColor: '#3E3498',  // This is the secondary theme color in _theme.scss
-        }]
-    },
-    options: {
-        scales: {
-            x: {
-                display: false
-            },
-            y: {
-                beginAtZero: true,
-                ticks: {
-                    callback: function(value, index, ticks) {
-                        if (value >= 1000) {
-                            return (value / 1000) + 'k';
-                        }
-                        else {
-                            return value;
+    const ctx = document.getElementById('activityChart');
+
+    chartInstance = new Chart(ctx, {
+        type: 'bar',
+        data: {
+            labels: hoverLabels,
+            datasets: [{
+                data: displaySessions,
+                borderWidth: 1,
+                backgroundColor: '#3E3498',  // This is the secondary theme color in _theme.scss
+            }]
+        },
+        options: {
+            scales: {
+                x: {
+                    display: false
+                },
+                y: {
+                    beginAtZero: true,
+                    ticks: {
+                        callback: function(value, index, ticks) {
+                            if (value >= 1000) {
+                                return (value / 1000) + 'k';
+                            }
+                            else {
+                                return value;
+                            }
                         }
                     }
-                }
+                },
             },
-        },
-        responsive: true,
-        maintainAspectRatio: false,
-        plugins: {
-            legend: {
-               display: false
-            },
-            tooltip: {
-                xAlign: 'center',
-                yAlign: 'bottom',
-                backgroundColor: '#FFFFFF',
-                bodyColor: '#000000',
-                titleColor: '#000000',
-                borderColor: '#3E3498',  // This is the secondary theme color in _theme.scss
-                borderWidth: 2,
-                displayColors: false,
-                titleFont: {weight: 'normal'},
-                bodyFont: {weight: 'bold'},
-                callbacks: {
-                    label: function(context) {
-                        let label = context.parsed.y + ' sessions';
-                        return label;
+            responsive: true,
+            maintainAspectRatio: false,
+            plugins: {
+                legend: {
+                display: false
+                },
+                tooltip: {
+                    xAlign: 'center',
+                    yAlign: 'bottom',
+                    backgroundColor: '#FFFFFF',
+                    bodyColor: '#000000',
+                    titleColor: '#000000',
+                    borderColor: '#3E3498',  // This is the secondary theme color in _theme.scss
+                    borderWidth: 2,
+                    displayColors: false,
+                    titleFont: {weight: 'normal'},
+                    bodyFont: {weight: 'bold'},
+                    callbacks: {
+                        label: function(context) {
+                            let label = context.parsed.y + ' sessions';
+                            return label;
+                        }
                     }
                 }
             }
         }
+    });
+}
+
+function updateChart(sessionsByDay, month) {
+    var hoverLabels = [];
+    var displaySessions = [];
+
+    for (const [key, value] of Object.entries(sessionsByDay)) {
+        var index = parseInt(key);
+        hoverLabels[index - 1] = month + " " + index;
+        displaySessions[index - 1] = value;
     }
-});
+
+    chartInstance.data.labels = hoverLabels;
+    chartInstance.data.datasets[0].data = displaySessions;
+    chartInstance.update();
+}
+
+export { createChart, updateChart }
