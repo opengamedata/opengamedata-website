d4cb6fd9e2099b21320ef61aef0de397f1157d87
mgsherry <michael.sherry@wisc.edu>
2023-05-09 10:55:22 -0500

    FD-43 additional path updates

--- site/components/card.php
+++ site/components/card.php
@@ -29,7 +29,7 @@ class Card
                                 <p class="card-subtitle small">' . htmlspecialchars($this->monthly_sessions) . ' Monthly Sessions</p>
                             </div>
                             <div class="card-footer d-flex align-items-center">
-                                <img class="avatar me-2" src="../assets/images/avatar.png" alt="avatar"> 
+                                <img class="avatar me-2" src="assets/images/avatar.png" alt="avatar"> 
                                 <h5 class="mb-0">' . htmlspecialchars($this->game->getDeveloperName()) . '</h5> 
                             </div>
                         </div>
