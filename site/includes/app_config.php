929a95e97f88b6c30a02fba15843661f197206a5
mgsherry <michael.sherry@wisc.edu>
2023-05-10 14:23:53 -0500

    FD-43 update config WEBSITE_API_URL_BASE for Github find/replace

--- site/includes/app_config.php
+++ site/includes/app_config.php
@@ -4,9 +4,9 @@ class AppConfig
 {
     public static function getConfig()
     {
-        // Default to values for the production environment
+        // Set Api Url base string (populated in Github actions)
         $APP_CONFIG = [];
-        $APP_CONFIG['WEBSITE_API_URL_BASE'] = 'https://ogd-services.fielddaylab.wisc.edu/wsgi-bin/opengamedata-website-api/production/app.wsgi/';
+        $APP_CONFIG['WEBSITE_API_URL_BASE'] = 'OGD_API_PATH_BASE';
 
         // If hostname starts with localhost
         if(!empty($_SERVER['HTTP_HOST']) && substr($_SERVER['HTTP_HOST'], 0, 9) === 'localhost')
