b694885c5a5e2ed612a3c76b21baace5fdf85f3a
mgsherry <michael.sherry@wisc.edu>
2023-06-22 10:25:02 -0500

    FD-48 add "published_in" property to Publications

--- site/models/publication.php
+++ site/models/publication.php
@@ -6,19 +6,21 @@ class Publication
     protected $name;
     protected $year;
     protected $project_code_link;
+    protected $published_in;
 
-    public function __construct($name=null, $paper_link=null, $authors=null, $year=null, $project_code_link=null) 
+    public function __construct($name=null, $paper_link=null, $authors=null, $year=null, $project_code_link=null, $published_in=null) 
     {
         $this->name = $name;
         $this->paper_link = $paper_link;
         $this->authors = $authors;
         $this->year = $year;
         $this->project_code_link = $project_code_link;
+        $this->published_in = $published_in;
     }
 
     public static function fromObj($obj) 
     {
-        return new static($obj->{'name'}, $obj->{'paper_link'}, implode(', ', $obj->{'authors'}), $obj->{'year'}, $obj->{'project_code_link'});
+        return new static($obj->{'name'}, $obj->{'paper_link'}, implode(', ', $obj->{'authors'}), $obj->{'year'}, $obj->{'project_code_link'}, $obj->{'published_in'});
     }
 
     public function getAuthors()
@@ -41,6 +43,10 @@ class Publication
     {
         return $this->project_code_link;
     }
+    public function getPublishedIn() 
+    {
+        return $this->published_in;
+    }
     public function getFormattedPublication()
     {
         $paperLink = htmlspecialchars($this->paper_link);
@@ -48,10 +54,12 @@ class Publication
         $pubName = htmlspecialchars($this->name);
         $pubYear = htmlspecialchars($this->year);
         $pubAuthors = htmlspecialchars($this->authors);
+        $publishedIn = htmlspecialchars($this->published_in);
         $formatted = '';
         if (!empty($this->authors)) $formatted.= "{$pubAuthors}. ";
         if (!empty($this->year)) $formatted.= "({$pubYear}). ";
         $formatted.= !empty($this->paper_link) ? "<a href=\"{$paperLink}\" target=\"_blank\">{$pubName}.</a> " : "{$pubName}. "; 
+        if (!empty($this->published_in)) $formatted.= "{$publishedIn}. ";
         if (!empty($this->project_code_link)) $formatted.= "<br><a class=\"btn btn-outline-secondary btn-publication mt-3\" href=\"{$projectLink}\" target=\"_blank\">View Project Code</a>";
         return $formatted;
     }
