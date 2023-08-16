363499b4b18ba04f6e11c04eafb4497eb9e9b9b5
mgsherry <michael.sherry@wisc.edu>
2023-08-16 13:32:49 -0500

    FD-58 CR changes - fix space in "reas chers"; text line-height, sizes, & spacing

--- site/about.php
+++ site/about.php
@@ -10,7 +10,7 @@
 </section>
 <main id="about">
     <section class="container-fluid mb-5">
-        <h2 class="text-sm-center">Open Science for Educational Games</h2>
+        <h2 class="text-sm-center pb-2">Open Science for Educational Games</h2>
         <p class="text-sm-center mx-sm-5 px-md-5 my-4">Open Science for Education Games<br />
            From the logging libraries that studios can integrate into their games, all the way to the final visualizations that allow design researchers to build new theories, we are thinking about modularity, scalability and performance.</p>
         <div class="row py-5">
@@ -42,7 +42,7 @@
             <div class="col-sm text-center p-5">
                 <img class="mb-2" src="assets/images/graphics/open-access.png" alt="">
                 <h3 class="h4">Open Access:</h3>
-                <p class="lead mx-3 mx-sm-0 mx-md-4">We encourage resear chers to make their research outputs available (freely) to everyone.</p>
+                <p class="lead mx-3 mx-sm-0 mx-md-4">We encourage researchers to make their research outputs available (freely) to everyone.</p>
             </div>
         </div>
         <div class="row py-5">
@@ -50,22 +50,22 @@
                 <img class="img-fluid-box mb-4 mb-lg-0" src="assets/images/graphics/why-open-data.png" alt="open data graphic">
             </div>
             <div class="col-sm order-sm-first my-auto">
-                <h2 class="mb-3">Why Open Data?</h2>
+                <h2 class="mb-4">Why Open Data?</h2>
                 <p class="me-md-5">Gone are the days of ad hoc, glued together analysis! Currently only large well funded groups can do this kind of research. It's time we worked together. Open data will support teams of various sizes and capacities. This project aims to expand who is able to participate.</p>
                 <p class="me-md-5">We hope to accelerate the velocity of research and promote better science. You can't replicate a study if the data is closed. Open science makes research processes transparent.</p>
                 <a href="getinvolved.php" class="btn btn-header-dark mt-3">Get Involved</a>
             </div>
         </div>
         <hr class="mt-4 mb-5" />
-        <h2 class="text-sm-center pt-5">Who Are We?</h2>
+        <h2 class="text-sm-center pt-5 pb-2">Who Are We?</h2>
         <p class="mx-sm-5 px-md-5 my-4">We are a consortium of researchers from around the country hosted by WCER's Field Day Lab. We are funded through individual research projects as well through a NSF coordinating network grant and support from the Learning Agency.</p>
         <div class="row py-sm-5">
             <div class="col-sm order-sm-last text-center">
                 <img class="staff-photo mb-2 mb-lg-0" src="assets/images/photos/david-gagnon.png" alt="photo of David Gagnon">
             </div>
             <div class="col-sm-7 order-sm-first">
-                <h4>David Gagnon</h4>
-                <h5>Project PI</h5>
+                <h3>David Gagnon</h3>
+                <h4 class="text-muted mb-4">Project PI</h4>
                 <p class="me-md-4">David Gagnon is director of Field Day Lab, an award-winning interdisciplinary team at the Wisconsin Center for Education Research at UW&ndash;Madison. He's also a game designer and passionate advocate for using games to understand and transform how kids learn. David is the co-founder of the PLAY MAKE LEARN conference and the Games and Learning Summit. He serves as a PI on several research projects that explore the following topics in video games: educational data mining, co-design, mixed reality (VR, AR), and location-based games.</p>
                 <p class="me-md-4">David created ARIS, an open-source game design platform that has supported the development of thousands of location-based AR projects and hundreds of educational papers. David's work supports other research, from the Open Game Data project, which shares our game data with educational researchers, to Field Day's teacher fellowship program, which serves as a site to study teachers' professional growth.</p>
             </div>
@@ -75,8 +75,8 @@
                 <img class="staff-photo mb-2 mb-lg-0" src="assets/images/photos/erik-harpstead.png" alt="photo of Erik Harpstead">
             </div>
             <div class="col-sm-7 order-sm-first">
-                <h4>Erik Harpstead</h4>
-                <h5>Co-PI</h5>
+                <h3>Erik Harpstead</h3>
+                <h4 class="text-muted mb-4">Co-PI</h4>
                 <p class="me-md-4">Erik Harpstead is a systems scientist in the Human-Computer Interaction Institute at Carnegie Mellon University (CMU), where he contributes to large research projects at the intersection of LE and game design. His work focuses on design and analysis of educational games as well as novel interactive machine learning techniques for modeling the human learning process in complex domains.</p>
                 <p class="me-md-4">He oversaw development of RI software projects such as Apprentice Learner Architecture, which provides tools for simulating experiments of the human learning process, and Twitch Research Toolkit, which enables researchers to execute studies on the game streaming platform Twitch.tv while abiding by relevant research ethics regulations.</p>
                 <p class="me-md-4">Over the past several years, he co-led the Computational Models of Learning track at the annual Simon Initiative LearnLab Summer School hosted at CMU, which introduces researchers, practitioners, and students to cutting-edge LE techniques with the goal of expanding the broader adoption of LE practices. Harpstead also teaches classes on design of educational games, covering the integration of pedagogical content and learning science research into complex game systems.</p>
@@ -87,8 +87,8 @@
                 <img class="staff-photo mb-2 mb-lg-0" src="assets/images/photos/magy-seif-el-nasr.png" alt="photo of Magy Seif El-Nasr">
             </div>
             <div class="col-sm-7 order-sm-first">
-                <h4>Magy Seif El-Nasr</h4>
-                <h5>Co-PI</h5>
+                <h3>Magy Seif El-Nasr</h3>
+                <h4 class="text-muted mb-4">Co-PI</h4>
                 <p class="me-md-4">Magy Seif El-Nasr is a professor at University of California&ndash;Santa Cruz with expertise in areas at the intersection of game design, artificial intelligence, human&ndash;computer interaction, and psychology.</p>
                 <p class="me-md-4">She has worked on systems that model the human emotion process and that model human behaviors in mobile games as well as serious games. She developed several innovative technologies to enable designers to track and understand human behavior through game data science. This led to several papers, four best paper awards, and two books: Game Analytics: Maximizing the Value of Player Data (2013, Springer) and Game Data Science (2012, Oxford University Press). She investigates how users can use data of their play and others' play in learning games to enhance self-regulated learning and reflection.</p>
             </div>
@@ -98,8 +98,8 @@
                 <img class="staff-photo mb-2 mb-lg-0" src="assets/images/photos/luke-swanson.png" alt="photo of Luke Swanson">
             </div>
             <div class="col-sm-7 order-sm-first">
-                <h4>Luke Swanson</h4>
-                <h5>Project Producer and Researcher</h5>
+                <h3>Luke Swanson</h3>
+                <h4 class="text-muted mb-4">Project Producer and Researcher</h4>
                 <p class="me-md-4">As a researcher and developer, Luke is drawn to the unique possibilities of games for learning. He is interested in designing data-driven tools that can help teachers and students alike gain more benefit from their use of games in the classroom. Luke's software engineering and data analysis work have allowed us to gain valuable insights into how our games are being used.</p>
                 <p class="me-md-4">In addition to developing infrastructure for Open Game Data, Luke has worked with Field Day on several projects, including a real-time game data dashboard for Lakeland, visualizing player activity in VR within Waddle: A Penguin's Tale, clustering players by playstyle for Lakeland, and studying the effect of different scripts in Jo Wilder and the Capitol Case.</p>
                 <p class="me-md-4">In his spare time, Luke is an amateur photographer and videographer. You can see some of his work on the Computer Science department's website.</p>
