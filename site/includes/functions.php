f0eb6d388987a359a3aae47aeadd20c2184cfb7d
Luke Swanson <lwswanson2@wisc.edu>
2024-06-13 20:48:29 -0500

    Add simple profiling functions.

--- site/includes/functions.php
+++ site/includes/functions.php
@@ -7,3 +7,22 @@ function num_in_kilo ( $num ) {
     if ($num < 1000) return $num;
     return round($num/1000) . "K"; 
 }
+
+/*
+ * Based on example at https://stackoverflow.com/questions/21133/simplest-way-to-profile-a-php-script
+ */
+function profile_point($msg) {
+    global $profiler_timing, $profiler_names;
+    $profiler_timing[] = microtime(true);
+    $profiler_names[] = $msg;
+}
+
+function profiler_print() {
+    global $profiler_timing, $profiler_names;
+    $count = count($profiler_timing);
+    for ($i = 0; $i < $count-1; $i++) {
+        echo "<b>{$profiler_names[$i]}</b><br>";
+        echo sprintf("&nbsp&nbsp;&nbsp;%f<br>", $profiler_timing[$i+1]-$profiler_timing[$i]);
+    }
+    echo "<b>{$profiler_names[$count-1]}</b><br>";
+}
