5add72723251fefc97fb56cbcd7686fe29a97859
Luke Swanson <lwswanson2@wisc.edu>
2024-07-12 15:18:27 -0500

    Move profiling code to own class.

--- site/includes/functions.php
+++ site/includes/functions.php
@@ -7,29 +7,3 @@ function num_in_kilo ( $num ) {
     if ($num < 1000) return $num;
     return round($num/1000) . "K"; 
 }
-
-/** Set next "breakpoint" for profiling.
- *  Based on example at https://stackoverflow.com/questions/21133/simplest-way-to-profile-a-php-script
- */
-function profile_point($msg) {
-    global $profiler_timing, $profiler_messages;
-    $profiler_timing[] = microtime(true);
-    $profiler_messages[] = $msg;
-}
-
-/** Print out all profiling points to a div.
- *  Based on example at https://stackoverflow.com/questions/21133/simplest-way-to-profile-a-php-script
- */
-function profiler_print() {
-    global $profiler_timing, $profiler_messages;
-    $final_timing = microtime(true);
-    $count = count($profiler_timing);
-    echo "<div>";
-    for ($i = 0; $i < $count-1; $i++) {
-        echo "<b>{$profiler_messages[$i]}</b>";
-        echo sprintf("&nbsp&nbsp;&nbsp;%f<br>", $profiler_timing[$i+1]-$profiler_timing[$i]);
-    }
-    echo "<b>{$profiler_messages[$count-1]}</b>";
-    echo sprintf("&nbsp&nbsp;&nbsp;%f<br>", $final_timing-$profiler_timing[$count-1]);
-    echo "</div>";
-}
---
+++ site/includes/profiler.php
@@ -0,0 +1,65 @@
+<?php
+
+class Profiler
+{
+   private $profiler_timing;
+   private $profiler_messages;
+   private $subprofiler = null;
+   private int $indent_level = 0;
+
+   public function __construct($indent_level)
+   {
+      $this->indent_level = $indent_level;
+   }
+
+   // Get methods
+   public function getIndent()
+   {
+      return $this->indent_level;
+   }
+   public function getSubprofiler()
+   {
+      return $this->subprofiler;
+   }
+
+   public function resetSubprofiler()
+   {
+      $this->getSubprofiler()->Complete();
+      return $this->subprofiler = new Profiler($this->getIndent() + 1);
+   }
+
+   /** Set next "breakpoint" for profiling.
+    *  Based on example at https://stackoverflow.com/questions/21133/simplest-way-to-profile-a-php-script
+    */
+   public function ProfilePoint($msg)
+   {
+      $this->profiler_timing[] = microtime(true);
+      $this->profiler_messages[] = $msg;
+   }
+
+   /** Print out all profiling points to a div.
+    *  Based on example at https://stackoverflow.com/questions/21133/simplest-way-to-profile-a-php-script
+    */
+   public function Complete()
+   {
+      $final_timing = microtime(true);
+      $count = count($this->profiler_timing);
+      echo "<div>";
+      for ($i = 0; $i < $count - 1; $i++)
+      {
+         $this->_printPoint($i, $this->profiler_timing[$i+1]);
+      }
+      $this->_printPoint($count-1, $final_timing);
+      echo "</div>";
+   }
+
+   private function _printPoint(int $i, $end_time)
+   {
+      $indentation = $this->getIndent() * 3;
+      $timing = $end_time - $this->profiler_timing[$i];
+      echo "<span style=\"margin-left:{$indentation}\">";
+      echo "<b>{$this->profiler_messages[$i]}</b>";
+      echo "&nbsp&nbsp;&nbsp;{$timing}<br>";
+      echo "</span>";
+   }
+}
