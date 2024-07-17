f576c79ee607505b3b35af43302ec1708e3fa1db
Luke Swanson <lwswanson2@wisc.edu>
2024-07-16 19:33:14 -0500

    Redo profiling again, let's just have an array.

--- site/index.php
+++ site/index.php
@@ -9,24 +9,27 @@ require_once 'models/game_card.php';
 require_once 'components/card.php';
 require_once 'includes/profiler.php';
 
-$profiler = new Profiler("Profile", 0);
-$subprofiler = new Profiler("SubProfile", 1);
+$profilers = [];
+$profilers[] = new Profiler("Profile", 0);
 
 // Get game list
-$profiler->ProfilePoint("Get game data list from server");
+$profilers[0]->ProfilePoint("Get game data list from server");
 $gamelist_json = services\getGameList();
 $gamelist = $gamelist_json ? json_decode($gamelist_json) : [];
 
 $games = [];
-$profiler->ProfilePoint("Process game list");
+$profilers[0]->ProfilePoint("Process game list");
+$i=0;
 foreach($gamelist as $key => $value)
 {
     // Get game usage from api for each game
-    $subprofiler->ProfilePoint("Get usage for game {$key}");
+    $profilers[] = new Profiler("{$key}Profile", 1);
+    $i++;
+    $profilers[$i]->ProfilePoint("Get usage for game {$key}");
     $response_obj = services\getGameUsage($key);
     $game_usage = null;
     if (isset($response_obj)) {
-        $subprofiler->ProfilePoint("Setup game usage object for {$key}");
+        $profilers[$i]->ProfilePoint("Setup game usage object for {$key}");
         $api_response = APIResponse::fromObj($response_obj);
         if ($api_response->Status() == "SUCCESS") {
             $game_usage = GameUsage::fromObj($api_response->Value());
@@ -37,16 +40,15 @@ foreach($gamelist as $key => $value)
         }
     }
     else {
-        $subprofiler->ProfilePoint("Handle error getting usage for game {$key}");
+        $profilers[$i]->ProfilePoint("Handle error getting usage for game {$key}");
         $err_str = "getGameUsage request, with game_id=".$key.", got no response object!";
         error_log($err_str);
     }
 
-    $subprofiler->ProfilePoint("Create game card object for game {$key}");
+    $profilers[$i]->ProfilePoint("Create game card object for game {$key}");
     $game_card = new GameCard(Game::fromJson($key, json_encode($value)), $game_usage);
     array_push($games, $game_card);
-    $subprofiler->Complete();
-    $subprofiler = new Profiler("SubProfile", 1);
+    $profilers[$i]->Complete();
 }
 ?>
 <?php require 'includes/header.php'; ?>
@@ -65,7 +67,7 @@ foreach($gamelist as $key => $value)
         <h2 class="mb-5 text-center">Featured Data Sets</h2>
         <div class="row row-cols-1 row-cols-sm-2 row-cols-md-3 row-cols-lg-4 g-4">
             <?php 
-                $profiler->ProfilePoint("Render game cards");
+                $profilers[0]->ProfilePoint("Render game cards");
                 foreach($games as $game_card) {
                     $card = new Card($game_card->getGame(),$game_card->getGameUsage());
                     echo $card->render();
@@ -75,4 +77,4 @@ foreach($gamelist as $key => $value)
     </section>
 </main>
 <!-- Begin Footer Include -->
-<?php require 'includes/footer.php'; $profiler->Complete();?>
+<?php require 'includes/footer.php'; $profilers[0]->Complete();?>
